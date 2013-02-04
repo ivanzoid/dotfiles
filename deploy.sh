e70282ec97575fb2c859766d580705db270afa34
Ivan Zezyulya <ivan@zezyulya.com>
2013-02-04 23:14:57 +0700

    Added snippets, update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-DOTFILES="$HOME/dotfiles"
+pushd ~ >/dev/null
 
-rm -rf $HOME/Library/Developer/Xcode/UserData/CodeSnippets
-mkdir -p $HOME/Library/Developer/Xcode/UserData
-ln -F -s $DOTFILES/Xcode/CodeSnippets $HOME/Library/Developer/Xcode/UserData
+rm -rf Library/Developer/Xcode/UserData/CodeSnippets
+mkdir -p Library/Developer/Xcode/UserData
+ln -sf dotfiles/Xcode/CodeSnippets Library/Developer/Xcode/UserData
 
-rm -rf $HOME/Library/Developer/Xcode/UserData/FontAndColorThemes
-mkdir -p $HOME/Library/Developer/Xcode/UserData
-ln -F -s $DOTFILES/Xcode/FontAndColorThemes $HOME/Library/Developer/Xcode/UserData
+rm -rf Library/Developer/Xcode/UserData/FontAndColorThemes
+mkdir -p Library/Developer/Xcode/UserData
+ln -sf dotfiles/Xcode/FontAndColorThemes Library/Developer/Xcode/UserData
+
+for f in dotfiles/.[^.]*; do
+	ln -sf "$f" .
+done
+
+popd >/dev/null
 
