1e5dcd26a2f64eefb989f746f42fc4823f47b7a2
Ivan Zezyulya <ivan@zezyulya.com>
2013-02-04 23:22:24 +0700

    alter deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,14 @@ pushd $HOME >/dev/null
 
 rm -rf Library/Developer/Xcode/UserData/CodeSnippets
 mkdir -p Library/Developer/Xcode/UserData
-ln -sf dotfiles/Xcode/CodeSnippets Library/Developer/Xcode/UserData
+ln -s -F dotfiles/Xcode/CodeSnippets Library/Developer/Xcode/UserData
 
 rm -rf Library/Developer/Xcode/UserData/FontAndColorThemes
 mkdir -p Library/Developer/Xcode/UserData
-ln -sf dotfiles/Xcode/FontAndColorThemes Library/Developer/Xcode/UserData
+ln -s -F dotfiles/Xcode/FontAndColorThemes Library/Developer/Xcode/UserData
 
 for f in dotfiles/.[^.]*; do
-	ln -sf "$f" .
+	ln -s -F "$f" .
 done
 
 popd >/dev/null
